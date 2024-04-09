0c0771f09d9c186d38e080072f0588eca5a0df1a
LilyBurton <lil@192.168.1.198>
2024-04-09 17:01:38 +0100

    Basic Card

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@
 import './App.css'
 
 function App() {
-
+  const title = "ToDo List"
+  const task = "Clean the kitchen"
+  const status = "Done"
   return (
-    <div className="text-3xl font-bold underline">
-      Hello
+    <div className="border rounded-lg px-2 m-2 bg-stone-400">
+      <div className="text-base font-semibold py-2">
+        {title}
+      </div>
+      <div className="flex gap-4 justify-between py-2">
+        <div>{task}</div>
+        <div>{status}</div>
+      </div>
+      <div>
+        
+      </div>
+      
     </div>
   )
 }
