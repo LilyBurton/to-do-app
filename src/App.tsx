4761534d55474e35fa087c84b35c52e5a6785e58
LilyBurton <lil@192.168.1.198>
2024-04-15 16:24:00 +0100

    Array of Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import './App.css'
 import TaskCard from './components/TaskCard'
-import {Task} from './utils/data-tasks'
+import {tasks} from './utils/data-tasks'
 
 function App() {
-  const task: Task = {
-    title: "Walk the dog",
-    notStarted: "Not Started",
-    inProgress: "In Progress",
-    status: "Done"
-  }
   
   return (
   <>
-    <TaskCard task={task} />
-    {/* <TaskCard title="Buy Groceries" notStarted={notStarted} inProgress={inProgress} status={status}/>
-    <TaskCard title="Cook Food" notStarted={notStarted} inProgress={inProgress} status={status}/> */}
+    {tasks.map((task) => <TaskCard task={task} />)}
     
   </>
   )
--- src/utils/data-tasks.tsx
+++ src/utils/data-tasks.tsx
@@ -4,3 +4,30 @@ export type Task = {
   inProgress: string,
   status: string
 }
+
+export const tasks: Task[] = [
+  {
+    title: "Walk the dog",
+    notStarted: "Not Started",
+    inProgress: "In Progress",
+    status: "Done"
+  },
+  {
+    title: "Buy Groceries",
+    notStarted: "Not Started",
+    inProgress: "In Progress",
+    status: "Done"
+  },
+  {
+    title: "Cook Food",
+    notStarted: "Not Started",
+    inProgress: "In Progress",
+    status: "Done"
+  },
+  {
+    title: "Exercise",
+    notStarted: "Not Started",
+    inProgress: "In Progress",
+    status: "Done"
+  }
+]
